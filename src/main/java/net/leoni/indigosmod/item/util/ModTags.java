525c2d370507da85c76c3696833f2d6df856aa62
Leoboi1xD <leonidas.anwar@gmail.com>
2026-01-11 22:11:47 +0100

    Created ModTags , important for next update
    Ive added tags for my mod , those will be important for the upcoming updates and releases to my mod , sorry for not releasing anything in a week

---
+++ src/main/java/net/leoni/indigosmod/item/util/ModTags.java
@@ -0,0 +1,26 @@
+package net.leoni.indigosmod.item.util;
+
+import net.leoni.indigosmod.IndigosMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> NEEDS_AMETHYST_INGOT_TOOL = tag("needs_amethyst_ingot_tool");
+
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(IndigosMod.MOD_ID, name));
+        }
+    }
+    public static class Items {
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(IndigosMod.MOD_ID, name));
+        }
+
+    }
+}
